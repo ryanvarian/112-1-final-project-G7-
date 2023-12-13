9c22998fdee901c7535aa4947ed558295bd657bd
ryanvarian <144878781+ryanvarian@users.noreply.github.com>
2023-12-13 13:52:50 +0800

    Update finalprojectg7.r

--- finalprojectg7.r
+++ finalprojectg7.r
@@ -2,6 +2,8 @@ library(readr)
 dq_download_csv <- read_csv("https://quality.data.gov.tw//dq_download_csv.php?nid=160174&md5_url=7de169d7b036bb2b0c7abb7546992ad8")
 View(dq_download)
 
+dplyr::glimpse(dq_download_csv)
+
 dq_download_csv$年度
 dq_download_csv$死亡原因
 dq_download_csv$`人數-男`
@@ -14,4 +16,14 @@ dq_download_csv$`給付金額-合計`
 dq_download_csv$給付金額比率
 
 data <- (dq_download_csv)
+
+# 把死亡人數性別為男者由高排到低
 sort_data <- data[order(data$`人數-男`), ]
+
+dq_download_csv$年度 |>
+ table() |>
+  sort(decreasing = TRUE)
+
+#找出最主要的死亡原因
+whichIsMax <- which.max(dq_download_csv$`人數-合計`)
+dq_download_csv$死亡原因[whichIsMax]
